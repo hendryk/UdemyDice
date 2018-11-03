05212a0d9d4c713c6b7fc4fa6f8f731358a35eb9
Jacek Krzyżanowski <krzyzakov@gmail.com>
2018-11-03 13:30:32 +0100

    wersja finalna

--- UdemyDice/ViewController.swift
+++ UdemyDice/ViewController.swift
@@ -4,22 +4,31 @@ class ViewController: UIViewController {
     
     var randomIndex1: Int = 0
     var randomIndex2: Int = 0
+    
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
 
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updateDiceImages()
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
+        updateDiceImages()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateDiceImages()
+    }
+    
+    func updateDiceImages () {
         randomIndex1 = Int.random(in: 0...5)
         randomIndex2 = Int.random(in: 0...5)
         
-        print(randomIndex1)
-        print(randomIndex2)
+        diceImageView1.image = UIImage(named: diceArray[randomIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomIndex2])
     }
-    
 }
 
